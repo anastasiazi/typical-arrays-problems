4549a25e5989d1a4f64b293f1180a11f4b53af70
anastasiazi <anastasiiiazi@gmail.com>
2021-03-04 16:41:39 +0200

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  let min;
+    for ( i = 0; i < array.length; i++) {
+        if (min === undefined || array[i] < min) {
+            min = array[i]
+        }
+    } 
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  let max;
+    for ( i = 0; i < array.length; i++) {
+        if (max === undefined || array[i] > max) {
+            max = array[i]
+        }
+    } 
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  let summ = 0;
+
+    for ( i = 0; i < array.length; i++) {
+        summ += array[i];
+    } 
+
+    return summ / array.length;
 }
